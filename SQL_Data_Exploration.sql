edf03a9cd35357a61c268c79b7bb71f84d8d633e
Stevenraynaldi <steven.raynaldi@gmail.com>
2022-06-08 12:59:04 +0800

    Update SQL_Data_Exploration.sql

--- SQL_Data_Exploration.sql
+++ SQL_Data_Exploration.sql
@@ -1,18 +1,33 @@
+-- Welcome to my covid 19 data exploration project using SQL
+-- My idea of doing this is to understand the effects of Covid 19 on the world and also specifically Singapore. 
+-- I will also be using different data exploration techniques to explore and transform the data in a way that will be useful for us.
+-- TLDR:
+	-- Part 1: The relationship between total case of Covid, deaths and infection rate due to covid from Singapore and around the world
+		-- Here I focus on exploration of data using Group by, Order by and manipulation of columns using functions such as Max and Sum.
+	-- Part 2: The relationship between covid and Vaccination
+		-- Here I used Joins, CTE, creating views and temp table
+
+--------------------------------------------------------------------The full data--------------------------------------------------------------------
 Select * From PortfolioProject..CovidDeath
 order by 3,4
 
+Select * From CovidVaccination
+order by 3,4
 
--- Select * From CovidVaccination
--- order by 3,4
 
+--------------------------------------------------------------------Part 1: Total covid case and death --------------------------------------------------------------------
 -- Select data that we are using
 Select location, date, total_cases, new_cases, total_deaths, population
 from PortfolioProject..CovidDeath
 Order by 1,2
 
+-- Quick look of Singapore cases on 7 May 2022
+-- Total Covid case:			1,215,499
+-- Number of Covid related Death:	1350
+
 
 -- Looking at total cases vs total deaths
--- Shows the likelyhood of dying if you contract  covid in Singapore
+-- As of 7 May 2022, In Singapore, if you contract covid, there is a 0.11% chance of dying
 Select location, date, total_cases, total_deaths, (total_deaths / total_cases)*100 as DeathPercentage 
 from PortfolioProject..CovidDeath
 Where continent is not null
@@ -21,7 +36,8 @@ Order by 1,2
 
 
 -- Look at total cases vs population
--- Shows what population got covid
+-- The infection rate Shows perfentage of population that got covid
+-- As of 7 May 2022, Singapore have infection rate of 22.29% (1,215,499 positive cases)
 Select location, date, total_cases,population, (total_cases / population)*100 as InfectedRate
 from PortfolioProject..CovidDeath
 Where continent is not null
@@ -30,7 +46,8 @@ Order by 1,2
 
 
 -- What country has the highest infectionRate
-Select location, population, Max(total_cases) as HighestInfectionCount, max(total_cases / population)*100 as InfectedRate, max(total_deaths / total_cases)*100 as DeathPercentage
+-- Faeroe Islands has the highest infection rate of 70.65%
+Select location, population, Max(total_cases) as HighestInfectionCount, max(total_cases / population)*100 as InfectedRate, max(total_deaths) /max( total_cases)*100 as DeathPercentage
 from PortfolioProject..CovidDeath
 Group By location, population
 Order by InfectedRate desc
@@ -38,16 +55,16 @@ Order by InfectedRate desc
 
 -- Showing countries witht he highest death count per population
 -- First problem encountered is data type of total_deaths
--- There are aggregated number in the data set
+-- There are aggregated number in the data set too hence needed to only look for data where continent is null
+-- United States has the highest death count from Covid of 997,503
 Select location, max(cast(total_deaths as int)) as TotalDeathCount
 from PortfolioProject..CovidDeath
 Where continent is not null
--- And location = 'Singapore'
 Group By location
 Order by TotalDeathCount desc
 
 
--- Let's break it down by continent
+-- Breaking it down it down by continent
 
 -- -- Showing continents with the highest death counts per population
 Select continent, max(cast(total_deaths as int)) as TotalDeathCount
@@ -57,21 +74,12 @@ Where continent is not null
 Group By continent
 Order by TotalDeathCount desc
 
-
--- Global Numbers
-
-Select date, Sum(new_cases) as GlobalNewCase, sum(cast(new_deaths as int)) as GlobalNewDeath, Sum(cast(new_deaths as int))/ Sum(New_cases)*100 as GlobalDeathPercentage
-From PortfolioProject..CovidDeath
-Where continent is not null
--- And location = 'Singapore'
-Group by date 
-Order by 1,2
-
-
-Select * From PortfolioProject ..CovidVaccination
-
+--------------------------------------------------------------------Part 2: Total covid case and Vaccination --------------------------------------------------------------------
 
 -- Looking at Total Population vs vaccination
+-- Here I am looking at new vaccination numbers by day and the total vaccinated after each new vaccination as total rolling vaccination numbers
+-- As of 7 May 2022, Singapore has 13,996,469 vaccination (both first vaccination and booster)
+-- What else I want to know is how quickly singapore manage to provide vaccinations to the whole population. hence the next part.
 Select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations
 , sum(cast(cv.new_vaccinations as int)) over (partition by cd.location order by cd.location, cd.date) as Total_Rolling_vaccinated
 From PortfolioProject..CovidDeath as cd
@@ -84,6 +92,8 @@ order by 1,2,3
 
 
 -- USE CTE
+-- The problem that I faced here is that I want to use created columns in another column. Hence, required to use CTE as a solution.
+-- Here we get to see Singapore has provided 255% vaccinations to their whole population. 
 with PopvsVac (Continent, Location, Date, Population, New_vaccinations, Total_Rolling_vaccinated)
 as
 (
@@ -99,25 +109,30 @@ and cd.location = 'Singapore'
 Select *, (Total_Rolling_vaccinated/population)*100 as Rolling_Vaccinated_Percentage
 From PopvsVac
 
+-- Looking at Global scale
+-- The top 5 countries with highest percentage of vacination per populations are: 
+-- 1. Cuba
+-- 2. Chile
+-- 3. Gibraltar
+-- 4. Singapore
+-- 5. South Korea
 
--- USE CTE (Self practice)
--- Showing death percentage per day vs number of vaccination
-with DeavsVac (Continent, Location, Date, Population, New_vaccinations, Total_Rolling_vaccinated, New_deaths, Total_Rolling_Deaths)
+with PopvsVac (Continent, Location, Date, Population, New_vaccinations, Total_Rolling_vaccinated)
 as
 (
 Select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations
 , sum(convert(bigint, cv.new_vaccinations)) over (partition by cd.location order by cd.location, cd.date) as Total_Rolling_vaccinated
-, cd.new_deaths, sum(convert(int, cd.new_deaths)) over (partition by cd.location order by cd.location, cd.date) as Total_Rolling_Deaths
 From PortfolioProject..CovidDeath as cd
 Join PortfolioProject ..CovidVaccination as cv
 	on cd.location = cv.location 
 	and cd.date = cv.date
 where cd.continent is not null
-and cd.location = 'Singapore'
+--and cd.location = 'Singapore'
 )
-Select Continent, location, date, (Total_Rolling_vaccinated/population)*100 as Rolling_Vaccinated_Percentage, (Total_Rolling_Deaths/population)*100 as Rolling_Deaths_Percentage
-From DeavsVac
-
+Select Continent, location,population, max(total_rolling_vaccinated) as max_vaccinated, max(Total_Rolling_vaccinated/population)*100 as Rolling_Vaccinated_Percentage
+From PopvsVac
+group by continent, location, population
+order by 5 desc
 
 -- Temp Table
 
